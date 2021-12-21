dcdafcce6b22fec0018078cfa15aa8f15a19fd20
vince du <haoyudu@hotmail.com>
2021-12-21 14:02:06 -0500

    update client with gatekeeper requests

--- client.py
+++ client.py
@@ -27,7 +27,7 @@ def initialize_socket(host, port):
 
 
 def send_write_requests(socket):
-    f = open('sakila-data-inventory-100.txt', 'r')
+    f = open('sakila-data-inventory-300.txt', 'r')
     for line in f:
         line = line.strip('\n')
         cmd = insert_cmd + line
@@ -38,7 +38,7 @@ def send_write_requests(socket):
 
 
 def send_read_requests(socket, mode):
-    nb_rows = 100
+    nb_rows = 300
     for i in range(1, nb_rows + 1):
         cmd = select_cmd + str(i)
         req = {'type': 'select', 'command': cmd, 'mode': mode}
@@ -52,28 +52,44 @@ def clean_database(socket):
     socket.recv(2048)
 
 def main():
+    # performance measurement for proxy server
     host = config['ProxyServer']['Host']
     port = int(config['ProxyServer']['Port'])
-    socket = initialize_socket(host, port)
-
-    # performance measurement for proxy server
+    proxy_socket = initialize_socket(host, port)
 
     # mode 0 for direct hit, 1 for random, 2 for customized
     for mode in range(3):
         print("Requests to proxy server with mode " + str(mode) + " started")
         start = time.time()
-        send_write_requests(socket)
-        send_read_requests(socket, mode)
+        send_write_requests(proxy_socket)
+        send_read_requests(proxy_socket, mode)
         request_time = time.time() - start
-        print("Requests to proxy server with mode " + str(mode) + " completed in {0:.0f}ms".format(request_time))
+        print("Requests to proxy server with mode " + str(mode) + " completed in {:0.2f}ms".format(request_time))
 
         time.sleep(1)
-        clean_database(socket)
+        clean_database(proxy_socket)
 
-    # TODO:performance measurement for gatekeeper server
+    proxy_socket.close()
+    print("proxy socket is closed on client")
 
-    socket.close()
-    print("socket is closed on client")
+    # performance measurement for gatekeeper server
+    # host = config['GatekeeperServer']['Host']
+    # port = int(config['GatekeeperServer']['Port'])
+    # gatekeeper_socket = initialize_socket(host, port)
+    #
+    # for mode in range(3):
+    #     print("Requests to gatekeeper server with mode " + str(mode) + " started")
+    #     start = time.time()
+    #     send_write_requests(gatekeeper_socket)
+    #     send_read_requests(gatekeeper_socket, mode)
+    #     request_time = time.time() - start
+    #     print("Requests to gatekeeper server with mode " + str(mode) + " completed in {:0.2f}ms".format(request_time))
+    #
+    #     time.sleep(1)
+    #     clean_database(gatekeeper_socket)
+    #
+    # gatekeeper_socket.close()
+    # print("gatekeeper socket is closed on client")
 
 
 if __name__ == '__main__':
